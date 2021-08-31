4d6f132ceca9b83ae8552ed2b91aa87d12c2a425
Blaster4385 <53873108+Blaster4385@users.noreply.github.com>
2021-08-31 15:57:25 +0530

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -2,7 +2,4 @@
 #
 # Copyright (C) 2020 azrim.
 # All rights reserved.
-
-wget $1
-curl --insecure --user v3nk4135h:$password -T PixelBlaster*.zip sftp://frs.sourceforge.net/home/frs/project/pixelblaster-os/$2/
-rm PixelBlaster*
+curl --insecure --user v3nk4135h:$password -T downloads/PixelBlaster*.zip sftp://frs.sourceforge.net/home/frs/project/pixelblaster-os/$2/
